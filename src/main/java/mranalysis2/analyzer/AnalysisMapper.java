b133f05f71d918b93c292c9b977133e4660b9a0f
Peeratham Techapalokul <tpeera4@vt.edu>
2016-04-20 15:36:37 -0400

    map-reduce analysis with mrunit

---
+++ src/main/java/mranalysis2/analyzer/AnalysisMapper.java
@@ -0,0 +1,51 @@
+package mranalysis2.analyzer;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import cs.vt.analysis.analyzer.AnalysisManager;
+import cs.vt.analysis.analyzer.analysis.AnalysisException;
+import cs.vt.analysis.analyzer.parser.ParsingException;
+
+//ref:http://appsintheopen.com/posts/40-unit-testing-map-reduce-programs-with-mrunit
+
+public class AnalysisMapper extends Mapper<Object, Text, LongWritable, Text> {
+
+	static enum ErrorCounter {
+		MISMATCHED_PROJECT_ID, ANALYSIS_FAILURE, PARSING_FAILURE
+	};
+
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+		AnalysisManager blockAnalyzer = new AnalysisManager();
+		JSONObject report = null;
+		LongWritable projectID = null;
+
+		try {
+			JSONObject record = (JSONObject) new JSONParser().parse(value.toString());
+			projectID = new LongWritable((Long) record.get("_id"));
+			String src = record.get("src").toString();
+			report = blockAnalyzer.analyze(src);
+		} catch (ParsingException e) {
+			context.getCounter(ErrorCounter.PARSING_FAILURE).increment(1);
+			e.printStackTrace();
+		} catch (AnalysisException e) {
+			context.getCounter(ErrorCounter.ANALYSIS_FAILURE).increment(1);
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (blockAnalyzer.getProjectID() != projectID.get()) {
+			context.getCounter(ErrorCounter.MISMATCHED_PROJECT_ID).increment(1);
+		} else if(report!=null) {
+			Text result = new Text(report.toJSONString());
+			context.write(projectID, result);
+		}
+	}
+}
---
+++ src/test/java/mranalysis2/analyzer/AnalysisMapperTest.java
@@ -0,0 +1,75 @@
+package mranalysis2.analyzer;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+
+import cs.vt.analysis.analyzer.AnalysisManager;
+import cs.vt.analysis.analyzer.Main;
+import cs.vt.analysis.analyzer.analysis.AnalysisException;
+import cs.vt.analysis.analyzer.parser.ParsingException;
+import mranalysis2.analyzer.AnalysisMapper.ErrorCounter;
+
+
+public class AnalysisMapperTest {
+	MapDriver<Object, Text, LongWritable, Text> mapDriver;
+	private String[] lines;
+	private AnalysisManager blockAnalyzer;
+	private JSONParser jsonParser;
+
+    @Before
+    public void setup() throws IOException {
+    	AnalysisMapper mapper = new AnalysisMapper();
+        mapDriver = MapDriver.newMapDriver(mapper);
+        blockAnalyzer = new AnalysisManager();
+        jsonParser = new JSONParser();
+    }
+    
+    @Test
+    public void testOutputIDandAnalysisResult() throws IOException, InterruptedException, ParsingException, AnalysisException, ParseException {
+    	InputStream in = Main.class.getClassLoader().getResource("hdfs_input_files/sources-0.json").openStream();
+    	lines = IOUtils.toString(in).split("\n");
+    	Text value = new Text(lines[0]);
+        
+        
+		JSONObject record = (JSONObject) jsonParser.parse(value.toString());
+		LongWritable projectID = new LongWritable( (Long) record.get("_id"));
+		String src = record.get("src").toString();
+        JSONObject report = blockAnalyzer.analyze(src);
+        
+        mapDriver.withInput(new LongWritable(), value)
+                .withOutput(projectID, new Text(report.toJSONString())) 
+                .runTest();
+    } 
+    
+    @Test
+    public void testNoOutputForMismatchedID() throws IOException, ParseException, ParsingException, AnalysisException{
+    	InputStream in = Main.class.getClassLoader().getResource("hdfs_input_files/mismatchedID.json").openStream();
+    	lines = IOUtils.toString(in).split("\n");    	
+    	Text value = new Text(lines[0]);
+        
+        
+		JSONObject record = (JSONObject) jsonParser.parse(value.toString());
+		LongWritable projectID = new LongWritable( (Long) record.get("_id"));
+		String src = record.get("src").toString();
+        JSONObject report = blockAnalyzer.analyze(src);
+        
+        mapDriver.withInput(new LongWritable(), value)
+                .runTest();
+        
+        assertEquals("Expected 1 counter increment", 1, mapDriver.getCounters()
+                .findCounter(ErrorCounter.MISMATCHED_PROJECT_ID).getValue());
+    }
+    
+}
